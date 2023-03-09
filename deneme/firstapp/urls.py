b1fbb5cc64b530011578170d88079d23a2c456a3
idrisabkar <idrisabkar2018@gmail.com>
2023-03-09 22:55:20 +0300

    deneme

--- deneme/firstapp/urls.py
+++ deneme/firstapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',view=views.home)
+    path('', view=views.home),
+    path('welcome/', view=views.welcome)
 ]
--- deneme/firstapp/views.py
+++ deneme/firstapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 
 def home(request):
     return HttpResponse("hello wordl")
+
+
+def welcome(request):
+    return render(request, "index.html")
