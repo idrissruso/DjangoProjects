010aaf2e8a4f238c0656d490291dbd29078b1238
idrisabkar <idrisabkar2018@gmail.com>
2023-03-12 10:00:30 +0300

    login

--- deneme/portfolio/urls.py
+++ deneme/portfolio/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', view=views.home),
-    path('login', view=views.welcome)
 ]
--- deneme/portfolio/views.py
+++ deneme/portfolio/views.py
@@ -4,13 +4,4 @@ from .models import User
 
 
 def home(request):
-    if request.method == "POST":
-        try:
-            user = User.objects.get(user_name=request.post("username"))
-        except :
-            return HttpResponse("<h1>Not Found</h1>")
-    return render(request, "login.html")
-
-
-def welcome(request):
     return render(request, "index.html")
