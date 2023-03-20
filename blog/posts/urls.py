9197d851e32beb7f04dd5086cedb404d76aee67f
idrisabkar <idrisabkar2018@gmail.com>
2023-03-20 11:33:39 +0300

    post_islemleri

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',view=views.home,name="home")
+    path('',view=views.home,name="home"),
+    path('new_post/',views.addPost,name = "new_post"),
+    path('post/<int:id>',views.post,name = "post")
+    
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html',{"posts" : [1,2,3,4,5]})
+
+def addPost(request):
+    return render(request,'newpost.html')
+
+def post(request):
+    return render(request,'post.html')
