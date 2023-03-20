2211c7f5222df81667ba95888fb7e6ef0ee2dd1c
idrisabkar <idrisabkar2018@gmail.com>
2023-03-20 21:45:25 +0300

    database

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Posts(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=500)
     author = models.CharField(max_length=50)
-    time = models.DateTimeField
+    content = models.CharField(max_length=10000,default="hello wordl")
+    time = models.DateTimeField(default=timezone.now)
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Posts
+
 
 # Create your views here.
 def home(request):
     return render(request,'index.html',{"posts" : [1,2,3,4,5]})
 
 def addPost(request):
+    if request.method == "POST":
+        data = request.POST
+        post = Posts(title = data["title"],author = data["author"],content = data["content"])
+        post.save()
+        return redirect("home")
     return render(request,'newpost.html')
 
 def post(request,id):
