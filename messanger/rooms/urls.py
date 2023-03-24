63303bf2120558d2abc00c7c7ab38b97cf072658
idrisabkar <idrisabkar2018@gmail.com>
2023-03-24 16:08:13 +0300

    templates

--- messanger/rooms/urls.py
+++ messanger/rooms/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home,name = "home")
+    path('',views.home,name = "home"),
+    path('login/',views.login,name = 'login'),
+    path('register/',views.register,name = 'register'),
+    path('chat/<str:room>',views.chat,name = 'chat')
 ]
--- messanger/rooms/views.py
+++ messanger/rooms/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def chat(request,room):
+    return render(request,'chat.html')
