05f3cbc27c4a4f102f3f6dd8cc31fb8e2556dc01
idrisabkar <idrisabkar2018@gmail.com>
2023-03-24 21:51:10 +0300

    models

--- messanger/rooms/models.py
+++ messanger/rooms/models.py
@@ -1,5 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-
+class Room(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    
+class Message(models.Model):
+    content = models.CharField(max_length=10000)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        blank=False
+    )
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.PROTECT,
+        blank=False
+        )
+    
+    
